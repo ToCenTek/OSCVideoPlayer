36213d4ac46197b1497fb2a6d01a4b2a2de0d580
余海闯 <yhc@yuhaichuangdeMacBook-Air.local>
2026-03-11 02:09:26 +0800

    Enable Windows Zeroconf with system dnsapi.dll

--- build_windows.bat
+++ build_windows.bat
@@ -6,7 +6,8 @@ set SRC=src\Player.cpp src\DisplayInfo.cpp src\Platform.cpp src\OSCServer.cpp sr
 
 echo Compiling...
 
-C:\msys64\mingw64\bin\g++ -O2 -std=c++17 -mconsole -Iinclude %SRC% -o build\oscplayer.exe -lws2_32 -lwinmm
+REM Use system dnsapi.dll and enable Bonjour
+C:\msys64\mingw64\bin\g++ -O2 -std=c++17 -mconsole -Iinclude -DHAVE_BONJOUR=1 %SRC% -o build\oscplayer.exe -lws2_32 -lwinmm -ldnsapi
 
 if exist build\oscplayer.exe (
     echo OK
