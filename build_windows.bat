4d6ce05ab481e03d9e23cb6323882e9ec30a22b8
余海闯 <yhc@yuhaichuangdeMacBook-Air.local>
2026-03-11 02:20:00 +0800

    fix: Windows build use system dnssd.dll for Zeroconf

--- build_windows.bat
+++ build_windows.bat
@@ -6,8 +6,16 @@ set SRC=src\Player.cpp src\DisplayInfo.cpp src\Platform.cpp src\OSCServer.cpp sr
 
 echo Compiling...
 
-REM Use system dnsapi.dll and enable Bonjour
-C:\msys64\mingw64\bin\g++ -O2 -std=c++17 -mconsole -Iinclude -DHAVE_BONJOUR=1 %SRC% -o build\oscplayer.exe -lws2_32 -lwinmm -ldnsapi
+REM Create import library for dnssd.dll if not exists
+if not exist libdnssd.a (
+    echo Creating libdnssd.a...
+    if not exist temp mkdir temp
+    (echo EXPORTS & echo DNSServiceAddRecord & echo DNSServiceBrowse & echo DNSServiceConstructFullName & echo DNSServiceCreateConnection & echo DNSServiceEnumerateDomains & echo DNSServiceGetAddrInfo & echo DNSServiceGetProperty & echo DNSServiceNATPortMappingCreate & echo DNSServiceProcessResult & echo DNSServiceQueryRecord & echo DNSServiceReconfirmRecord & echo DNSServiceRefDeallocate & echo DNSServiceRefSockFD & echo DNSServiceRegister & echo DNSServiceRegisterRecord & echo DNSServiceRemoveRecord & echo DNSServiceResolve & echo DNSServiceUpdateRecord) > temp\dnssd.def
+    C:\msys64\mingw64\bin\dlltool.exe -D C:\Windows\System32\dnssd.dll -d temp\dnssd.def -l libdnssd.a
+)
+
+REM Compile with Bonjour support using system dnssd.dll
+C:\msys64\mingw64\bin\g++ -O2 -std=c++17 -mconsole -Iinclude -DHAVE_BONJOUR=1 %SRC% -o build\oscplayer.exe -lws2_32 -lwinmm libdnssd.a
 
 if exist build\oscplayer.exe (
     echo OK
