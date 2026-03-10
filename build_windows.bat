069084f19ca496a9ac5cb23e2fa4077cf6d61905
余海闯 <yhc@yuhaichuangdeMacBook-Air.local>
2026-03-11 01:25:26 +0800

    Fix Windows build script - add build dir and Zeroconf

--- build_windows.bat
+++ build_windows.bat
@@ -1,10 +1,12 @@
 @echo off
 
-set SRC=src\Player.cpp src\DisplayInfo.cpp src\Platform.cpp src\OSCServer.cpp src\OSCMessage.cpp src\AudioDevice.cpp src\main.cpp
+if not exist build mkdir build
+
+set SRC=src\Player.cpp src\DisplayInfo.cpp src\Platform.cpp src\OSCServer.cpp src\OSCMessage.cpp src\AudioDevice.cpp src\Zeroconf.cpp src\main.cpp
 
 echo Compiling...
 
-C:\msys64\mingw64\bin\g++ -O2 -std=c++17 -mconsole -Iinclude %SRC% -o build\oscplayer.exe -lws2_32 -lwinmm
+C:\msys64\mingw64\bin\g++ -O2 -std=c++17 -mconsole -Iinclude %SRC% -o build\oscplayer.exe -lws2_32 -lwinmm -ldnsapi
 
 if exist build\oscplayer.exe (
     echo OK
